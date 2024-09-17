6498c3c4a86526193bd680ab9b2189dc30262a3a
majesticr <sraikov7@gmail.com>
2024-09-17 12:43:56 +0400

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("123456")
+	currentTime := time.Now()
+	fmt.Println("Время: ", currentTime.String())
+	var (
+		a int     = 1
+		b float64 = 3.14
+		c string  = "word"
+		d bool    = true
+	)
+	e := "Text"
+
+	fmt.Println("int:", a)
+	fmt.Println("float64:", b)
+	fmt.Println("string:", c)
+	fmt.Println("bool:", d)
+	fmt.Println("short variable:", e)
+
 }
